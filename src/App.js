4a7d921cc297c12f69944276a114f79346eabefe
justinalize <justinalize@gmail.com>
2020-12-10 14:46:34 -0500

    DAY TWO

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import axios from 'axios'
 import { API_KEY, BASE_URL } from './key'
 import Title from './components/title'
 import Body from './components/body'
+import styled from 'styled-components'
 // API FETCH URL:   https://api.nasa.gov/planetary/apod?api_key=6RFlse80rtHVmkZNrtevyALHsEsvKa5yzFcDb4eJ&date=2020-12-07
+
+// COME BACK LATER AND ADD A CONDITION STATEMENT SO IF THE MEDIA TYPE IS A VIDEOP, PLAY THE VIDEO AND IF ITS A PHOTO DISPLAY THE PHOTO
+
+// ADD MEDIA QUIERIES WHEN YOU COME BACK TO FIX IT UP
 
 
 function App() {
@@ -13,7 +18,7 @@ function App() {
 
   useEffect(() => {
     const fetchImage = () => {
-      axios.get(`${BASE_URL}?api_key=${API_KEY}&date=2020-12-07`)
+      axios.get(`${BASE_URL}?api_key=${API_KEY}`)
         .then(response => { 
           console.log(response.data)
           setData(response.data)
--- src/components/title.js
+++ src/components/title.js
@@ -1,11 +1,37 @@
 import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+
+
+const StyledTitle = styled.div`
+color: black;
+margin: 0 auto;
+display:flex;
+flex-direction: column;
+text-align:right;
+width: 25%;
+left: 59%;
+position: absolute;
+top:30%;
+p{
+    text-align:center;
+    display:flex;
+    align-self:center;
+    font-size: 1.2rem;
+    font-weight: bold;
+}
+h1 {
+    text-align:center;
+}
+`
+
 
 export default function Title(props) {
 
     return (
-        <div className='title-container'>
-            <p>{props.data.title}</p>
-        </div>
+        <StyledTitle >
+            <h1>NASA Photo of the Day</h1>
+          <p>{props.data.title}:</p>
+        </StyledTitle>
 
     )
 }
